10c071c4d23680e5a8bf6aa29c7c5e9f2910e89c
Mary Okhtienko <mariohtienko@gmail.com>
2024-12-05 12:37:24 +0300

    Add initial project setup, Maven configuration and GitHub workflows

---
+++ bot/src/main/java/com/java/bot/BotApplication.java
@@ -0,0 +1,17 @@
+package com.java.bot;
+
+
+import com.java.bot.configuration.ApplicationConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+@EnableConfigurationProperties(ApplicationConfig.class)
+public class BotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
+}
