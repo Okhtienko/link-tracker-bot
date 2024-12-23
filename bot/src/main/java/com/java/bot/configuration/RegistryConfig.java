add5f6dd3b147a5da70d00de05be6bbf03b1428a
Mary Okhtienko <mariohtienko@gmail.com>
2024-12-24 00:29:53 +0700

    Rename CommandConfig.java

--- bot/src/main/java/com/java/bot/configuration/RegistryConfig.java
+++ bot/src/main/java/com/java/bot/configuration/RegistryConfig.java
@@ -1,17 +1,24 @@
 package com.java.bot.configuration;
 
 import com.java.bot.processor.Command;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CommandConfig {
+public class MapConfig {
 
     @Bean
     public Map<String, Command> commandMap(List<Command> commands) {
         return commands.stream().collect(Collectors.toMap(Command::command, command -> command));
+    }
+
+    @Bean
+    public Map<Long, Set<String>> linkMap() {
+        return new HashMap<>();
     }
 }
