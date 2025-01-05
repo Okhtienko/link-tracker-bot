b76ad5c8ada46ccf3e43a5bcff3e03d792d0e798
Mary Okhtienko <mariohtienko@gmail.com>
2025-01-05 16:39:28 +0700

    Update MenuHandler.java

--- bot/src/main/java/com/java/bot/handler/MenuHandler.java
+++ bot/src/main/java/com/java/bot/handler/MenuHandler.java
@@ -1,6 +1,6 @@
 package com.java.bot.handler;
 
-import com.java.bot.processor.Command;
+import com.java.bot.bot.Command;
 import com.pengrad.telegrambot.model.BotCommand;
 import org.springframework.stereotype.Component;
 
